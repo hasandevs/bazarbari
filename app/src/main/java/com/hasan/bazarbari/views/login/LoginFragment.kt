bb1485e89ca3cbe2648a97dbcf71824736b13f73
Hema <hhhsoftbd@gmail.com>
2024-01-24 19:22:04 +0600

    mvvm added

---
+++ app/src/main/java/com/hasan/bazarbari/views/login/LoginFragment.kt
@@ -0,0 +1,26 @@
+package com.hasan.bazarbari.views.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hasan.bazarbari.R
+
+
+class LoginFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
--- app/src/main/java/com/hasan/bazarbari/views/stater/MainActivity.kt
+++ app/src/main/java/com/hasan/bazarbari/views/stater/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hasan.bazarbari
+package com.hasan.bazarbari.views.stater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hasan.bazarbari.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
